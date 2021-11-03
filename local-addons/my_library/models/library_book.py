17edb45379f285432b838b7bad201c8e21d06909
Chris <chris@tasksuite.com>
2021-11-03 15:20:06 -0500

    security clearcnce added

--- local-addons/my_library/models/library_book.py
+++ local-addons/my_library/models/library_book.py
@@ -1,9 +1,8 @@
-from odoo import models, fields 
+from odoo import models, fields
+
+
 class LibraryBook(models.Model):
-    _name = 'library.book' 
-    name = fields.Char('Title', required=True) 
-    date_release = fields.Date('Release Date')
-    author_ids = fields.Many2many(
-        'res.partner',
-        string='Authors'
-    )
+    _name = "library.book"
+    name = fields.Char("Title", required=True)
+    date_release = fields.Date("Release Date")
+    author_ids = fields.Many2many("res.partner", string="Authors")
