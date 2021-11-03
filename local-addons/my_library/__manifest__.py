9761e43c136f33146e0ab333ceea371e40f89a88
Chris <chris@tasksuite.com>
2021-11-03 12:52:17 -0500

    book model added

---
+++ local-addons/my_library/__manifest__.py
@@ -0,0 +1,12 @@
+{
+ 'name': "My library",
+ 'summary': "Manage books easily",
+ 'description': """Long description""",
+ 'author': "Your name",
+ 'website': "http://www.example.com",
+ 'category': 'Uncategorized',
+ 'version': '12.0.1',
+ 'depends': ['base'],
+#  'data': ['views.xml'],
+ 'demo': ['demo.xml'],
+}
