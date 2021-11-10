09f3a2287daf9da50730d171f9141a79be2f34fa
Chris <chris@tasksuite.com>
2021-11-10 15:03:29 -0600

    added movie substraction

--- local-addons/mr_video/models/rentals.py
+++ local-addons/mr_video/models/rentals.py
@@ -1,4 +1,6 @@
+from typing_extensions import Required
 from odoo import models, fields, api
+from . import vhs
 
 class Rental(models.Model):
     _name = 'rental'
@@ -12,6 +14,51 @@ class Rental(models.Model):
         'vhs.movie', domain=[('quantity', '>', 0)], string='VHS rented'
     )
     
-    check_out = fields.Datetime(string="Checked Out", default=fields.Datetime.now, required=True)
+    # vhs_rental = fields.Many2many(
+    #     'vhs.movie', string='VHS rented', compute='_add'
+    # )
+    
+    
+    
+    check_out_date = fields.Datetime(string="Checked Out", default=fields.Datetime.now, required=True)
     
     is_returned = fields.Boolean('Returned', default=False)
+    
+    rent_selected = fields.Boolean('Rent selected VHS', default=False, required=True)
+    
+    @api.onchange('rent_selected')
+    def check_rented_true(self):
+        if self.rent_selected == True:
+            ids = []
+            for rec in self:
+                for item in rec.vhs_rental:
+                    ids.append(item.id)
+                    item.write({"quantity": item.quantity -1})
+                    print(item.name)
+                    print(item.quantity)
+                # Credit to Eric for below code
+                rec.vhs_rental= [(6, 0, ids)] 
+                    
+                    
+# print(self.env['vhs.movie'].search([('id', '=', )]))
+            
+           
+        
+        
+        
+    # def _add(self):
+    #     print("Hello from a function")
+    
+    
+    
+    # def add(self):
+    #     self.vhs_rental.add_quantity()
+    
+
+    def _add(self):
+        print("Hello from a function")
+ 
+    
+    # @api.depends('first_name', 'last_name')
+    # def _get_name(self):
+    #  for 
--- local-addons/mr_video/models/vhs.py
+++ local-addons/mr_video/models/vhs.py
@@ -3,6 +3,7 @@ import logging
 
 from odoo import models, fields, api
 from odoo.exceptions import UserError
+from odoo.tools.date_utils import subtract
 from odoo.tools.translate import _
 
 
@@ -11,58 +12,73 @@ logger = logging.getLogger(__name__)
 
 class VhsMovie(models.Model):
     _name = 'vhs.movie'
-    _description = 'Mr Video VHS Movies'
-
+    _description = 'Mr Video VHS Movies'   
+    
     name = fields.Char('Title', required=True)
-    date_release = fields.Date('Release Date')
-    # date_updated = fields.Datetime('Last Updated', copy=False)
-    # director_ids = fields.Many2many('res.partner', string='Directors')
+    date_release = fields.Date('Release Date')    
     director = fields.Char('Director')
     genre = fields.Char('Genre')
-    # genre_id = fields.Many2one('vhs.movie.genre', string='Genre')
     poster =fields.Binary(string='Movie Poster')
+    quantity = fields.Integer('Amount in Stock', required=True)    
+    state = fields.Selection(
+        [('available', 'Available'),
+         ('rented', 'Rented')],
+        'State', default="available")
     
-    # category = fields.Selection([
-    #     ('contract', 'Contract'),
-    #     ('service', 'Service')
-    #     ], 'Category', required=True)
-    
-    state = fields.Selection([
-        ('draft', 'Unavailable'),
-        ('available', 'Available'),
-        ('rented', 'Rented'),
-        ('lost', 'Lost')],
-        'State', default="draft")
-    
-    quantity = fields.Integer('Amount in Stock', required=True)
-
-    @api.model
-    def is_allowed_transition(self, old_state, new_state):
-        allowed = [('draft', 'available'),
-                   ('available', 'rented'),
-                   ('rented', 'available'),
-                   ('available', 'lost'),
-                   ('rented', 'lost'),
-                   ('lost', 'available')]
-        return (old_state, new_state) in allowed
-
-    @api.multi
-    def change_state(self, new_state):
-        for movie in self:
-            if movie.is_allowed_transition(movie.state, new_state):
-                movie.state = new_state
-            else:
-                message = _('Moving from %s to %s is not allowd') % (movie.state, new_state)
-                raise UserError(message)
-
     def make_available(self):
-        self.change_state('available')
+        self.ensure_one()
+        self.state = 'available'
 
     def make_rented(self):
-        self.change_state('rented')
+        self.ensure_one()
+        self.state = 'rented'
+           
+    def add_quantity(self):
+        added_quantity = self.quantity + 1
+        return added_quantity
 
-    def make_lost(self):
-        self.change_state('lost')
+    def subtrat_quantity(self):
+        subtracted_quantity = self.quantity - 1
+        return subtracted_quantity
+
+    # def book_rent(self):
+    #     self.ensure_one()
+    #     if self.state != 'available':
+    #         raise UserError(_('VHS is not available for renting'))
+    #     rent_as_superuser = self.env['library.book.rent'].sudo()
+    #     rent_as_superuser.create({
+    #         'book_id': self.id,
+    #         'borrower_id': self.env.user.partner_id.id,
+    #     })
+    
+
+    # @api.model
+    # def is_allowed_transition(self, old_state, new_state):
+    #     allowed = [('draft', 'available'),
+    #                ('available', 'rented'),
+    #                ('rented', 'available'),
+    #                ('available', 'lost'),
+    #                ('rented', 'lost'),
+    #                ('lost', 'available')]
+    #     return (old_state, new_state) in allowed
+
+    # @api.multi
+    # def change_state(self, new_state):
+    #     for movie in self:
+    #         if movie.is_allowed_transition(movie.state, new_state):
+    #             movie.state = new_state
+    #         else:
+    #             message = _('Moving from %s to %s is not allowd') % (movie.state, new_state)
+    #             raise UserError(message)
+
+    # def make_available(self):
+    #     self.change_state('available')
+
+    # def make_rented(self):
+    #     self.change_state('rented')
+
+    # def make_lost(self):
+    #     self.change_state('lost')
 
     # def create_genres(self):
     #     categ1 = {
@@ -125,9 +141,9 @@ class VhsMovie(models.Model):
     #     movies_directors = self.get_director_names(all_movies)
     #     logger.info('Movies Director: %s', movies_directors)
 
-    @api.model
-    def get_director_names(self, all_movies):
-        return all_movies.mapped('director_ids.name')
+    # @api.model
+    # def get_director_names(self, all_movies):
+    #     return all_movies.mapped('director_ids.name')
 
 
 # class VhsMember(models.Model):
@@ -139,6 +155,28 @@ class VhsMovie(models.Model):
 #     date_end = fields.Date('Termination Date')
 #     member_number = fields.Char()
 #     date_of_birth = fields.Date('Date of birth')
+
+
+# class Rental(models.Model):
+#     _name = 'rental'
+#     _description = 'VHS Rentals'
+ 
+#     name = fields.Many2one(
+#         'member', string='Member'
+#     )
+    
+#     vhs_rental = fields.Many2many(
+#         'vhs.movie', domain=[('quantity', '>', 0)], string='VHS rented'
+#     )
+    
+#     check_out = fields.Datetime(string="Checked Out", default=fields.Datetime.now, required=True)
+    
+#     is_returned = fields.Boolean('Returned', default=False)
+    
+#     def add(self):
+#         return VhsMovie.add_quantity
+
+
 
 def my_function():
   print("Hello from a function")
